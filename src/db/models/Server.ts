459d65a781e9ce35a5e0a2e7fe886880242081aa
Jake Wymer <jakeandrewwymer@gmail.com>
2021-07-11 19:56:36 -0500

    set ablility to set sheets id

---
+++ src/db/models/Server.ts
@@ -0,0 +1,23 @@
+import sqlize from "sequelize";
+import db from "../index.js";
+const { DataTypes } = sqlize;
+
+const Server = db.define(
+  `server`,
+  {
+    server_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sheets_id: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    tableName: `server_config`,
+  }
+);
+
+Server.sync({ alter: true });
+
+export default Server;
