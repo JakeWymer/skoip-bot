1505912127e9c00e6a00732cf2acd92861252a81
Beugh <jpugh12@live.com>
2021-07-20 06:28:38 -0500

    finished slash commands

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,15 @@ dotenv.config();
 
 import {
   Client,
-  Message,
   TextChannel,
   GuildMember,
   VoiceChannel,
-  Guild,
   WSEventType,
 } from "discord.js";
 import MusicPlayer from "./MusicPlayer.js";
 import SpotifyGenerator from "./SpotifyGenerator.js";
 import { getRandomPlaylist } from "./sheets.js";
-import { TrackGenerator } from "./types.js";
+import { TrackGenerator, Commands } from "./types.js";
 import { getRandomElement, setupSpotifyApi } from "./util.js";
 import { EventEmitter } from "events";
 import Server from "./db/models/Server.js";
@@ -22,21 +20,6 @@ import "./db/index.js";
 import axios from "axios";
 
 const client = new Client();
-
-const BOT_PREFIX = `>`;
-
-const BotCommands = {
-  PLAY: [`play`, `p`],
-  SKIP: [`skoip`, `skip`, `s`],
-  KICK: [`kick`, `k`],
-  SHUFFLE: [`shuffle`],
-  QUEUE_RANDOM: [`queue_random`, `qr`, `random`],
-  CLEAR_QUEUE: [`clear`],
-  STOP: [`stop`],
-  SHOW_QUEUE: [`queue`, `q`],
-  LEAVE: [`leave`],
-  SET_SHEET: [`set_sheet_id`, `set_sheet`],
-};
 
 const botServerMap: { [key: string]: MusicPlayer } = {};
 
@@ -63,11 +46,6 @@ const getJoinMessage = (member: GuildMember) => {
   return randomMessage;
 };
 
-const matchCommand = (message: Message, command: string[]) => {
-  const content = message.content.slice(1).split(` `);
-  return command.includes(content[0]);
-};
-
 const isInServer = (guildId: string) => {
   return botServerMap[guildId];
 };
@@ -87,80 +65,25 @@ const leaveServer = (guildId: string) => {
   delete botServerMap[guildId];
 };
 
-const joinChannel = async (message: Message) => {
-  const sentBy = message.member as GuildMember;
-  const textChannel = message.channel as TextChannel;
-  const guild = message.guild as Guild;
-  if (!guild) {
-    return textChannel.send("No guild found.");
+const setSheetId = async (
+  sheetsId: string,
+  guildId: string,
+  channel: TextChannel
+) => {
+  let serverConfig = (await Server.findOne({
+    where: {
+      server_id: guildId,
+    },
+  })) as any;
+  if (!serverConfig) {
+    serverConfig = await Server.create({
+      server_id: guildId,
+    });
   }
-  if (!sentBy) {
-    return textChannel.send("Bot must join by member request.");
-  }
-  const voiceChannel = sentBy.voice.channel as VoiceChannel;
-  if (!voiceChannel) {
-    return textChannel.send("You need to be in a voice channel to play music.");
-  }
-  textChannel.send(getJoinMessage(sentBy));
-  const voiceConnection = await voiceChannel.join();
-  const em = new EventEmitter();
-  em.on("leave", leaveServer);
-  const player = new MusicPlayer(
-    voiceChannel,
-    textChannel,
-    voiceConnection,
-    em
-  );
-  botServerMap[guild.id] = player;
-  return player;
+  serverConfig.sheets_id = sheetsId;
+  serverConfig.save();
+  return channel.send(`Set sheets id to: ${sheetsId}`);
 };
-
-// const handleMessage = async (message: Message) => {
-//   if (message.content.length === 0 || message.content[0] !== BOT_PREFIX) return;
-//   const { guild } = message;
-//   if (!guild) {
-//     return message.channel.send("No guild found.");
-//   }
-//   let player = await getPlayer(message, guild.id);
-//   if (!player) {
-//     return;
-//   }
-
-// const textChannel = message.channel as TextChannel;
-
-//   if (matchCommand(message, BotCommands.PLAY)) {
-//     await handlePlayCommand(message, player);
-//   } else if (matchCommand(message, BotCommands.SHUFFLE)) {
-//     player.shuffle();
-//   } else if (matchCommand(message, BotCommands.SKIP)) {
-//     player.playNext(true);
-//   } else if (matchCommand(message, BotCommands.QUEUE_RANDOM)) {
-//     await handleQueueRandomCommand(textChannel, player);
-//   } else if (matchCommand(message, BotCommands.CLEAR_QUEUE)) {
-//     player.clearQueue();
-//   } else if (matchCommand(message, BotCommands.STOP)) {
-//     player.stop();
-//   } else if (matchCommand(message, BotCommands.SHOW_QUEUE)) {
-//     await player.showQueue();
-//   } else if (matchCommand(message, BotCommands.LEAVE)) {
-//     await player.leave();
-//   } else if (matchCommand(message, BotCommands.SET_SHEET)) {
-//     const sheetsId = message.content.split(` `)[1];
-//     let serverConfig = (await Server.findOne({
-//       where: {
-//         server_id: message.guild?.id,
-//       },
-//     })) as any;
-//     if (!serverConfig) {
-//       serverConfig = await Server.create({
-//         server_id: message.guild?.id,
-//       });
-//     }
-//     serverConfig.sheets_id = sheetsId;
-//     serverConfig.save();
-//     return message.channel.send(`Set sheets id to: ${sheetsId}`);
-//   }
-// };
 
 const joinChannelInteraction = async (
   sentBy: GuildMember,
@@ -187,7 +110,6 @@ const joinChannelInteraction = async (
 
 const handleInteraction = async (interaction: any) => {
   const responseURL = `https://discord.com/api/v8/interactions/${interaction.id}/${interaction.token}/callback`;
-  console.log(interaction);
   const guild = await client.guilds.fetch(interaction.guild_id);
   const channel = (await client.channels.fetch(
     interaction.channel_id
@@ -195,11 +117,33 @@ const handleInteraction = async (interaction: any) => {
   const user = await guild.members.fetch(interaction.member.user.id);
   const player = await getPlayer(user, channel, guild.id);
   switch (interaction.data.name) {
-    case "random":
+    case Commands.QUEUE_RANDOM:
       await handleQueueRandomCommand(channel, player);
       break;
-    case "skip":
+    case Commands.SKIP:
       player.playNext(true);
+      break;
+    case Commands.SHUFFLE:
+      player.shuffle();
+      break;
+    case Commands.CLEAR_QUEUE:
+      player.clearQueue();
+      break;
+    case Commands.SHOW_QUEUE:
+      await player.showQueue();
+      break;
+    case Commands.LEAVE:
+      await player.leave();
+      break;
+    case Commands.PLAY:
+      await handlePlayCommand(
+        interaction.data.options[0].value,
+        channel,
+        player
+      );
+      break;
+    case Commands.SET_SHEET:
+      await setSheetId(interaction.data.options[0].value, guild.id, channel);
       break;
     default:
       channel.send("Command not found");
@@ -210,11 +154,14 @@ const handleInteraction = async (interaction: any) => {
   });
 };
 
-const handlePlayCommand = async (message: Message, player: MusicPlayer) => {
-  const url = message.content.split(` `)[1];
+const handlePlayCommand = async (
+  url: string,
+  channel: TextChannel,
+  player: MusicPlayer
+) => {
   let generator: TrackGenerator;
   if (!url.includes(`spotify`)) {
-    return message.channel.send("Unsupported integration");
+    return channel.send("Unsupported integration");
   }
   const spotifyApi = await setupSpotifyApi();
   generator = new SpotifyGenerator(url, spotifyApi);
@@ -249,7 +196,6 @@ client.on("ready", () => {
   console.log("Skoipy online");
 });
 
-// client.on("message", handleMessage);
 const interactionEvent = "INTERACTION_CREATE" as WSEventType;
 client.ws.on(interactionEvent, handleInteraction);
 client.login(process.env.BOT_TOKEN || ``);
--- src/scripts/populate-slash-commands.mjs
+++ src/scripts/populate-slash-commands.mjs
@@ -2,29 +2,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import axios from "axios";
-import commands from "../commands.js";
+import commands from "../commands.mjs";
 
 const applicationId = process.env.APPLICATION_ID;
 const botToken = process.env.BOT_TOKEN;
 const url = `https://discord.com/api/v8/applications/${applicationId}/commands`;
 const guildURL = `https://discord.com/api/v8/applications/${applicationId}/guilds/596370362422395042/commands`;
 
-await Promise.all(
-  commands.map(async (command) => {
-    console.log(command);
-    const options = {
-      method: "post",
-      headers: { Authorization: `Bot ${botToken}` },
-      data: command,
-      url: guildURL,
-    };
-    await axios(options);
-  })
-);
 const options = {
-  method: "get",
-  headers: { Authorization: `Bot ${botToken}` },
+  method: "put",
+  headers: {
+    Authorization: `Bot ${botToken}`,
+    "Content-Type": "application/json",
+  },
+  data: JSON.stringify(commands),
   url: guildURL,
 };
-const currentCommands = await axios(options);
-console.log(currentCommands);
+try {
+  await axios(options);
+} catch (error) {
+  console.log(error.toJSON());
+}
